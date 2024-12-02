0e4ed7a9c63fa51e6a46843846d18122ccd0ad8b
Rohan Sharma <rohansh.lko@gmail.com>
2024-12-02 11:01:59 +0530

    added logs

--- src/routeWorker.ts
+++ src/routeWorker.ts
@@ -1,8 +1,10 @@
 const { parentPort, workerData } = require('worker_threads');
+import logger from './logger';
 import System from './models/System';
 
 async function performRouteCalculation() {
   const { origin, destination, waypoints, useThera, useTurnur, keepWaypointsOrder, minWhSize, avoidSystems, avoidEdencom, avoidTrig } = workerData;
+  logger.info(`Performing route calculation for ${origin} to ${destination} with waypoints ${waypoints.join(', ')} and options ${useThera}, ${useTurnur}, ${keepWaypointsOrder}, ${minWhSize}, ${avoidSystems}, ${avoidEdencom}, ${avoidTrig}`);
   try {
     const route = await System.getRoute(origin, destination, waypoints, useThera, useTurnur, keepWaypointsOrder,avoidSystems, avoidEdencom, avoidTrig, minWhSize);
     parentPort.postMessage({ route });
